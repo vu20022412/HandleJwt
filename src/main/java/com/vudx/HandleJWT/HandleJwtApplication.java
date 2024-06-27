49908fd5be659c70eaa6a672af39df4e221e1f86
vu20022412 <vu20022412@gmail.com>
2024-06-27 15:09:27 +0700

    Inittalization project

---
+++ src/main/java/com/vudx/HandleJWT/HandleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.vudx.HandleJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vudx/HandleJWT/HandleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vudx.HandleJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
